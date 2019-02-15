f20dce9f87f654cd9628dd7c67e6b7ce7f46fd69
yuanjie <yuanjie@shanghai3h.com>
2019-02-15 13:07:17 +0800

    init: 初始化项目，完成项目框架搭建

---
+++ src/metronic.js
@@ -0,0 +1,22 @@
+import './assets/metronic/vendors/perfect-scrollbar/css/perfect-scrollbar.css'
+import './assets/metronic/vendors/flaticon/css/flaticon.css'
+import './assets/metronic/vendors/metronic/css/styles.css'
+import './assets/metronic/css/style.bundle.css'
+import './assets/metronic/css/custom.css'
+
+import jQuery from 'jquery/dist/jquery'
+window.$ = jQuery
+window.jQuery = jQuery
+window.jquery = jQuery
+
+import 'bootstrap/dist/js/bootstrap'
+
+import './assets/metronic/js/framework/base/app'
+import './assets/metronic/js/framework/base/util'
+import './assets/metronic/js/framework/components/general/header'
+import './assets/metronic/js/framework/components/general/menu'
+import './assets/metronic/js/framework/components/general/offcanvas'
+import './assets/metronic/js/framework/components/general/scroll-top'
+import './assets/metronic/js/framework/components/general/toggle'
+import './assets/metronic/js/framework/components/general/dropdown'
+import './assets/metronic/js/demo/base/layout'
---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = (env, argv) => {
+	const devMode = argv.mode !== 'production'
+	return {
+		entry: {
+			metronic: './src/metronic.js',
+			main: ['@babel/polyfill', './src/index.js']
+		},
+		output: {
+			path: path.resolve('./dist'),
+			publicPath: '/'
+		},
+		module: {
+			rules: [{
+					test: /\.html$/,
+					use: [{
+						loader: "html-loader",
+						options: {
+							minimize: true
+						}
+					}]
+				},
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
+					options: {
+						loaders: {}
+					}
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
+					}
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+						'css-loader',
+						'postcss-loader',
+						'sass-loader',
+					]
+				},
+				{
+					test: /\.css$/,
+					use: [
+						devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+						'css-loader',
+						'postcss-loader'
+					]
+				},
+				{
+					test: /\.(png|jpg|jpeg|gif)(\?.*)?$/,
+					use: [{
+						loader: 'file-loader',
+						options: { 
+							name: 'assets/images/[name].[ext]'
+						}
+					}]
+				},
+				{
+					test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
+					use: [{
+						loader: 'file-loader',
+						options: {
+							name: 'assets/fonts/[name].[ext]'
+						}
+					}]
+				}
+			]
+		},
+		plugins: [
+			new VueLoaderPlugin(),
+			new CleanWebpackPlugin(['dist']),
+			new MiniCssExtractPlugin({
+				filename: "[name].css",
+				chunkFilename: "[id].css"
+			}),
+			new HtmlWebPackPlugin({
+				filename: "index.html",
+				template: "./src/index.html",
+				minify: {
+					removeComments: !devMode,
+					collapseWhitespace: !devMode
+				}
+			}),
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery',
+				jquery: 'jquery',
+			})
+		],
+		devtool: 'eval-source-map',
+		devServer: {
+			contentBase: path.resolve(__dirname, 'dist'),
+			inline: true,
+			historyApiFallback: true,
+			port: 3000
+		},
+		resolve: {
+			extensions: ['.js', '.vue', '.css'],
+			alias: {								
+				'assets': path.resolve(__dirname, './src/assets'),
+				'metronic': path.resolve(__dirname, './src/assets/metronic'),
+			}
+		}		
+	}
+}
